c7dcbed80ac06d0b9401aac18f06fc9a894d8c3b
Pursheld <stompee@bk.ru>
2021-04-06 22:27:18 +0300

    Добавьте файлы проекта.

---
+++ Homework2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <stdint.h>
+#include <iomanip>
+
+int main()
+{
+	std::string name;
+
+
+	name = "max";
+
+	std::cout << name << "\n";
+
+	std::cout << name.length()<< "\n";
+	
+	std::cout << name[0] << "\n";
+	
+	std::cout << name[2] << "";
+	
+}
